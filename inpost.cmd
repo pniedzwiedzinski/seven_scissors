6c97d60ef090da7f7edcbcb8cefbd230b529fb95
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2022-03-04 13:44:12 +0100

    Remove temp files
    Remove temp files before proceeding and add \s for rmdir

--- inpost.cmd
+++ inpost.cmd
@@ -1,4 +1,7 @@
 REM @echo off
+rmdir /s /q C:\Users\Praca\AppData\Local\Temp\seven_scissors
+del C:\Users\Praca\AppData\Local\Temp\page-*-*.png
+del C:\Users\Praca\AppData\Local\Temp\page-*-*.png.ele
 mkdir C:\Users\Praca\AppData\Local\Temp\seven_scissors
 
 "C:\Program Files\gs\gs9.55.0\bin\gswin64c.exe" -dNOPAUSE -dBATCH -sDEVICE=png16m -g2000x2828 -dPDFFitPage -dUseCropBox -sOutputFile="C:\Users\Praca\AppData\Local\Temp\seven_scissors\page-%%d.png" %1
@@ -10,10 +13,6 @@ for %%f in (C:\Users\Praca\AppData\Local\Temp\page-*-*.png) do (
 )
 
 magick convert C:\Users\Praca\AppData\Local\Temp\page-*-*.png.ele  -page A4+0+410 C:\Users\Praca\AppData\Local\Temp\etykieta-do-druku.pdf
-
-del C:\Users\Praca\AppData\Local\Temp\page-*-*.png
-del C:\Users\Praca\AppData\Local\Temp\page-*-*.png.ele
-rmdir /q C:\Users\Praca\AppData\Local\Temp\seven_scissors
 
 C:\Users\Praca\AppData\Local\Temp\etykieta-do-druku.pdf
 
